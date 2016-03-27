327ea18abe182a72ec1b5ce3d33b587a2bd0f2e6
Ed Caicedo <ed@edcaicedo.com>
2016-03-27 16:33:46 -0500

    Added test for multiple results

--- DnsRip.Tests/Tests/ResolverShould.cs
+++ DnsRip.Tests/Tests/ResolverShould.cs
@@ -48,6 +48,39 @@ namespace DnsRip.Tests.Tests
                             RecordIsIp = true
                         }
                     }
+                },
+                new ResolveTest
+                {
+                    Query = "fd-fp3.wg1.b.yahoo.com",
+                    Type = DnsRip.QueryType.A,
+                    IsRecursive = true,
+                    Expected = new List<TestResponse>
+                    {
+                        new TestResponse
+                        {
+                            Host = "fd-fp3.wg1.b.yahoo.com.",
+                            Type = DnsRip.QueryType.A,
+                            RecordIsIp = true
+                        },
+                        new TestResponse
+                        {
+                            Host = "fd-fp3.wg1.b.yahoo.com.",
+                            Type = DnsRip.QueryType.A,
+                            RecordIsIp = true
+                        },
+                        new TestResponse
+                        {
+                            Host = "fd-fp3.wg1.b.yahoo.com.",
+                            Type = DnsRip.QueryType.A,
+                            RecordIsIp = true
+                        },
+                        new TestResponse
+                        {
+                            Host = "fd-fp3.wg1.b.yahoo.com.",
+                            Type = DnsRip.QueryType.A,
+                            RecordIsIp = true
+                        }
+                    }
                 }
             };
 
@@ -61,18 +94,26 @@ namespace DnsRip.Tests.Tests
         public void Resolve(ResolveTest resolveTest)
         {
             var dnsRip = new DnsRip.Resolver(resolveTest);
-            var result = dnsRip.Resolve().First();
-            var expected = (TestResponse)resolveTest.Expected.First();
+            var resultSet = dnsRip.Resolve();
+            var expectedSet = resolveTest.Expected.ToList();
+            var index = 0;
 
-            Assert.That(result.Host, Is.EqualTo(expected.Host));
-            Assert.That(result.Type, Is.EqualTo(expected.Type));
-            Assert.That(DnsRip.Utilities.IsInteger(result.Ttl), Is.EqualTo(expected.TtlIsInteger));
+            foreach (var result in resultSet)
+            {
+                var expected = (TestResponse)expectedSet[index];
 
-            if (expected.RecordIsIp.HasValue)
-                Assert.That(DnsRip.Utilities.IsIp(result.Record), Is.EqualTo(expected.RecordIsIp));
+                Assert.That(result.Host, Is.EqualTo(expected.Host));
+                Assert.That(result.Type, Is.EqualTo(expected.Type));
+                Assert.That(DnsRip.Utilities.IsInteger(result.Ttl), Is.EqualTo(expected.TtlIsInteger));
 
-            if (expected.RecordIsHostname.HasValue)
-                Assert.That(DnsRip.Utilities.IsHostname(result.Record), Is.EqualTo(expected.RecordIsHostname));
+                if (expected.RecordIsIp.HasValue)
+                    Assert.That(DnsRip.Utilities.IsIp(result.Record), Is.EqualTo(expected.RecordIsIp));
+
+                if (expected.RecordIsHostname.HasValue)
+                    Assert.That(DnsRip.Utilities.IsHostname(result.Record), Is.EqualTo(expected.RecordIsHostname));
+
+                index++;
+            }
         }
     }
 }
